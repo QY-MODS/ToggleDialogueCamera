a09963bec57dd1928bf1316b037c9cb2c4e335d9
Ege Yilmaz <a.egeyilmaz@hotmail.com>
2023-08-05 06:02:08 +0200

    Update plugin.cpp

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -1,76 +1,20 @@
 #include "logger.h"
 
-bool toggle_pressed = false;
-
-class OnCameraUpdate {
-public:
-    static void Install() {
-        REL::Relocation<std::uintptr_t> hook1{REL::RelocationID(49852, 50784)};  // 84AB90, 876700
-
-        auto& trampoline = SKSE::GetTrampoline();
-        trampoline.create(16);
-        _Update = trampoline.write_call<5>(hook1.address() + REL::Relocate(0x1A6, 0x1A6),
-                                           Update);  // 84AD36, 8768A6
-    }
-
-private:
-    static void Update(RE::TESCamera* a_this);
-
-    static inline REL::Relocation<decltype(Update)> _Update;
-};
-void OnCameraUpdate::Update(RE::TESCamera* a_camera) {
-    _Update(a_camera);
-    if (toggle_pressed && RE::PlayerCamera::GetSingleton()->IsInThirdPerson()) {
-        logger::info("In Hook.");
-        auto* thirdPersonState = static_cast<RE::ThirdPersonState*>(a_camera->currentState.get());
-        logger::info("savedZoomOffset: {} pitchZoomOffset: {}, currentZoomOffset: {}, targetZoomOffset: {}",
-                     thirdPersonState->savedZoomOffset, thirdPersonState->pitchZoomOffset,
-                     thirdPersonState->currentZoomOffset, thirdPersonState->targetZoomOffset);
-        if (thirdPersonState->currentZoomOffset != 0.0f) {
-            thirdPersonState->targetZoomOffset = 0.0f;
-        }
-        toggle_pressed = false;
-    }
-    //if (toggle_pressed) {
-        /*logger::info("{} {} {}", a_camera->cameraRoot->local.translate.x, a_camera->cameraRoot->local.translate.y,
-                     a_camera->cameraRoot->local.translate.z);
-        logger::info("{} {} {}", a_camera->cameraRoot->local.rotate.entry[0][0],
-                     a_camera->cameraRoot->local.rotate.entry[0][1], a_camera->cameraRoot->local.rotate.entry[0][2]);*/
-        /*a_camera->cameraRoot->local.translate += {105.0f, 105.0f, 105.0f};
-        RE::NiUpdateData updateData;
-        a_camera->cameraRoot->UpdateDownwardPass(updateData, 0);*/
-        //toggle_pressed = false;
-    //}
-}
 
 RE::UI* ui = nullptr;
 RE::PlayerCamera* player_cam = nullptr;
-RE::PlayerControls* player_controls = nullptr;
 RE::BSInputDeviceManager* input_device_manager = nullptr;
 const std::string_view& dialogue_menu_str = "Dialogue Menu";
-constexpr std::uint32_t toggle_code_keyboard = 47;
+constexpr std::uint32_t toggle_code_keyboard = 33;
 constexpr std::uint32_t toggle_code_gamepad = 128;
+constexpr std::uint32_t zoom_code_keyboard = 29;
+constexpr std::uint32_t zoom_code_gamepad = 64;
 bool PostPostLoaded = false;
 bool InputLoaded = false;
+bool zoom_enabled = false;
 
-//float GetZoomDistance(RE::PlayerCamera* plyr_c) {
-//    plyr_c->cameraRoot->local.translate;
-//    return 0;
-//}
-    //void ToggleDialogueCam(RE::PlayerCamera* plyr_c) {
-//    if (plyr_c->IsInFirstPerson()) {
-//        // logger::info("Player is in 1st person.");
-//        plyr_c->ForceThirdPerson();
-//    } else if (plyr_c->IsInThirdPerson()) {
-//        plyr_c->ForceFirstPerson();
-//        // logger::info("Player is in 3rd person.");
-//    } else {
-//        logger::info("Player is in neither 1st nor 3rd person.");
-//    }
-//};
-
-class OurEventSink : public RE::BSTEventSink<SKSE::CameraEvent>,
-                     public RE::BSTEventSink<RE::InputEvent*> {
+using InputEvents = RE::InputEvent*;
+class OurEventSink : public RE::BSTEventSink<RE::InputEvent*> {
     OurEventSink() = default;
     OurEventSink(const OurEventSink&) = delete;
     OurEventSink(OurEventSink&&) = delete;
@@ -83,77 +27,47 @@ public:
         return &singleton;
     }
 
-    RE::BSEventNotifyControl ProcessEvent(const SKSE::CameraEvent* a_event, RE::BSTEventSource<SKSE::CameraEvent>*) {
-        if (!a_event) return RE::BSEventNotifyControl::kContinue;
-        if (!a_event->oldState) return RE::BSEventNotifyControl::kContinue;
-        if (!a_event->newState) return RE::BSEventNotifyControl::kContinue;
-        if (toggle_pressed) return RE::BSEventNotifyControl::kContinue;
+    RE::BSEventNotifyControl ProcessEvent(const InputEvents* evns, RE::BSTEventSource<InputEvents>*) {
+	if (!*evns)
+		return RE::BSEventNotifyControl::kContinue;
 
-        /*if (a_event->oldState->id == RE::CameraStates::kThirdPerson) {
-            logger::info("Third person camera state.");
-            logger::info("{} {} {}", a_event->oldState->camera->cameraRoot->local.translate.x,
-                         a_event->oldState->camera->cameraRoot->local.translate.y,
-                         a_event->oldState->camera->cameraRoot->local.translate.z);
-            logger::info("{} {} {}", a_event->oldState->camera->cameraRoot->local.rotate.entry[0][0],
-                         a_event->oldState->camera->cameraRoot->local.rotate.entry[0][1],
-                         a_event->oldState->camera->cameraRoot->local.rotate.entry[0][2]);
-        }*/
-        return RE::BSEventNotifyControl::kContinue;
-    }
+	for (RE::InputEvent* e = *evns; e; e = e->next) {
+		switch (e->eventType.get()) {
+            case RE::INPUT_EVENT_TYPE::kButton :
+				RE::ButtonEvent* a_event = e->AsButtonEvent();
 
-    RE::BSEventNotifyControl ProcessEvent(RE::InputEvent* const* eventPtr, RE::BSTEventSource<RE::InputEvent*>*) {
-        if (!eventPtr) return RE::BSEventNotifyControl::kContinue;
+				uint32_t keyMask = a_event->idCode;
 
-        auto* event = *eventPtr;
-        if (!event) return RE::BSEventNotifyControl::kContinue;
+				float duration = a_event->heldDownSecs;
+				bool isPressed = a_event->value != 0 && duration >= 0;
+				bool isReleased = a_event->value == 0 && duration != 0;
 
-        // if (!(ui->IsMenuOpen(dialogue_menu_str))) return RE::BSEventNotifyControl::kContinue;
-        if (event->GetEventType() != RE::INPUT_EVENT_TYPE::kButton) return RE::BSEventNotifyControl::kContinue;
-        auto* buttonEvent = event->AsButtonEvent();
-        if (!(buttonEvent->IsUp())) return RE::BSEventNotifyControl::kContinue;
-        bool _toggle = false;
-        auto dxScanCode = buttonEvent->GetIDCode();
-
-        // 0 = keyboard,1 = mouse,  2 = gamepad
-        int _device = event->GetDevice();
-
-        if (_device == 0 && dxScanCode == toggle_code_keyboard) {
-            _toggle = true;
-        } else if (_device == 2 && dxScanCode == toggle_code_gamepad) {
-            _toggle = true;
-        }
-        if (_toggle) {
-            player_cam = RE::PlayerCamera::GetSingleton();
-            logger::info("Toggle pressed.");
-            auto thirdPersonState =
-                static_cast<RE::ThirdPersonState*>(player_cam->cameraStates[RE::CameraState::kThirdPerson].get());
-            logger::info("THIRD PERSON STATE");
-            logger::info("savedZoomOffset: {} pitchZoomOffset: {}, currentZoomOffset: {}, targetZoomOffset: {}",
-                         thirdPersonState->savedZoomOffset, thirdPersonState->pitchZoomOffset,thirdPersonState->currentZoomOffset, thirdPersonState->targetZoomOffset);
-            logger::info("posOffsetActual:");
-            LogNP3(thirdPersonState->posOffsetActual);
-            logger::info("posOffsetExpected:");
-            LogNP3(thirdPersonState->posOffsetExpected);
-            logger::info("rotation:");
-            LogNP4(thirdPersonState->rotation);
-            logger::info("translation:");
-            LogNP3(thirdPersonState->translation);
-
-            logger::info("FIRST PERSON STATE");
-            auto firstPersonState = static_cast<RE::FirstPersonState*>(player_cam->cameraStates[RE::CameraState::kFirstPerson].get());
-            logger::info("currentPitchOffset: {} targetPitchOffset: {}, sittingRotation: {}",
-			        firstPersonState->currentPitchOffset, firstPersonState->targetPitchOffset, firstPersonState->sittingRotation);
-            logger::info("lastPosition:");
-            LogNP3(firstPersonState->lastPosition);
-            // ToggleDialogueCam(player_cam);
-            toggle_pressed = !toggle_pressed;
-            player_cam = nullptr;
-        }
-        
-        
-
-        return RE::BSEventNotifyControl::kContinue;
-    }
+				if (isPressed) {
+                    if ((keyMask == zoom_code_keyboard || keyMask == zoom_code_gamepad) && !zoom_enabled) {
+                        zoom_enabled = true;
+                        continue;
+                    }
+                    if (zoom_enabled){
+                        if (keyMask == 8) {
+                            auto thirdPersonState = static_cast<RE::ThirdPersonState*>(
+                                RE::PlayerCamera::GetSingleton()->cameraStates[RE::CameraState::kThirdPerson].get());
+                            thirdPersonState->targetZoomOffset = std::max(thirdPersonState->targetZoomOffset - 0.1f, -0.2f);
+                        } else if (keyMask == 9) {
+                            auto thirdPersonState = static_cast<RE::ThirdPersonState*>(
+                                RE::PlayerCamera::GetSingleton()->cameraStates[RE::CameraState::kThirdPerson].get());
+                            thirdPersonState->targetZoomOffset = std::min(thirdPersonState->targetZoomOffset + 0.1f, 1.0f);
+                        }
+                    }
+				}
+				else if (isReleased) {
+                    if (keyMask == zoom_code_keyboard || keyMask == zoom_code_gamepad) {
+                        zoom_enabled = false;
+                    }
+				}
+		}
+	}
+	return RE::BSEventNotifyControl::kContinue;
+}
 };
 
 auto* eventSink = OurEventSink::GetSingleton();
@@ -168,7 +82,6 @@ void OnMessage(SKSE::MessagingInterface::Message* message) {
         case SKSE::MessagingInterface::kPostPostLoad:
             if (!PostPostLoaded) {
                 ui = RE::UI::GetSingleton();
-                SKSE::GetCameraEventSource()->AddEventSink(eventSink);
                 PostPostLoaded = true;
             }
             break;
@@ -179,7 +92,6 @@ SKSEPluginLoad(const SKSE::LoadInterface* skse) {
     SKSE::Init(skse);
     SetupLog();
     SKSE::GetMessagingInterface()->RegisterListener(OnMessage);
-    OnCameraUpdate::Install();
 
     return true;
 }
